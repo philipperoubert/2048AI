2318ee6e516ec5becbdf2d2da0fd9491bb555493
Philippe <philippe@roubert.email>
2020-01-11 13:38:50 +0100

    Cleaned up the code

--- monte_carlo_simulator.py
+++ monte_carlo_simulator.py
@@ -1,9 +1,8 @@
-from monte_carlo_board import Board
 import os
-from termcolor import colored
-import numpy as np
-import random
 from multiprocessing import Pool
+import random
+from termcolor import colored
+from board import Board
 
 def beautify_print(board):
     """
@@ -12,22 +11,23 @@ def beautify_print(board):
         board: 4x4 np array representing the board.
     """
 
-    color_dict = {0: "red", 2: "green", 4: "yellow", 8: "blue", 16: "magenta", 32: "cyan", 64: "green",
-                  128: "yellow", 256: "blue", 512: "magenta", 1024: "cyan", 2048: "green", 5096: "yellow"}
+    color_dict = {0: "red", 2: "green", 4: "yellow", 8: "blue", 16: "magenta",
+                  32: "cyan", 64: "green", 128: "yellow", 256: "blue", 512: "magenta",
+                  1024: "cyan", 2048: "green", 5096: "yellow"}
     print("==================")
-    for i in range(4):
+    for row in range(4):
         print("|", end=" ")
-        for j in range(3):
+        for collumn in range(3):
             try:
-                print(
-                    colored(str(int(board[i][j])), color_dict[int(board[i][j])]), end=" | ")
-            except:
-                print(board[i][j], end=" | ")
+                print(colored(str(int(board[row][collumn])),
+                              color_dict[int(board[row][collumn])]), end=" | ")
+            except IndexError:
+                print(board[row][collumn], end=" | ")
         try:
-            print(colored(str(int(board[i][3])),
-                          color_dict[int(board[i][3])]), end=" |\n")
-        except:
-            print(board[i][3], end=" |\n")
+            print(colored(str(int(board[row][3])),
+                          color_dict[int(board[row][3])]), end=" |\n")
+        except IndexError:
+            print(board[row][3], end=" |\n")
         print("==================")
 
 def cls():
@@ -38,10 +38,15 @@ def cls():
 
 
 def simulate_run(board):
+    """
+    Will simulate a board game by making random moves.
+    params:
+        board: board object instance
+    """
     newer_board = Board(board.board, board.points)
     first_iteration = True
-    
     first_move = None
+
     while newer_board.moves_available():
         available_moves = newer_board.moves_available(True)  # Gets a list of all possible moves
         move_to_make = random.choice(available_moves)
@@ -53,7 +58,9 @@ def simulate_run(board):
 
 if __name__ == "__main__":
 
-    for _ in range(5):
+    number_of_games = 5
+
+    for _ in range(number_of_games):
 
         board1 = Board()  # Initialise a board
         number_of_processors = 4 # For multiprocessing
@@ -67,8 +74,8 @@ if __name__ == "__main__":
         while board1.moves_available():
             cls() # comment this out if you want the program to print out everything
             beautify_print(board1.board)
-            depth = 1 # number of games being played
-            final_scores = {"w":[0,0], "a":[0,0], "s":[0,0], "d":[0,0],}
+            depth = 500 # number of games being played
+            final_scores = {"w":[0, 0], "a":[0, 0], "s":[0, 0], "d":[0, 0],}
 
             with Pool(number_of_processors) as p:
                 scores = p.map(simulate_run, [board1] * depth)
@@ -81,13 +88,13 @@ if __name__ == "__main__":
             for i in final_scores:
                 try:
                     final_scores[i][0] /= final_scores[i][1]
-                except:
+                except ZeroDivisionError:
                     final_scores[i][0] = 0
                 if final_scores[i][0] > best_score:
                     best_score = final_scores[i][0]
                     best_move = i
 
-            with open("dataset.txt", "a") as file:
+            with open("datasetTest.txt", "a") as file:
                 file.write(str(best_move + "," + str(list(board1.board.flatten())) + "\n"))
             file.close()
             board1.make_move(best_move)
