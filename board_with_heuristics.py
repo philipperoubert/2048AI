7525caf31225bb11f57d860da579d45c2475c00e
PeterAndrewGilbert <peter.a.gilbert98@gmail.com>
2020-01-12 22:56:53 +0000

    added expecimax data generation code. needs PEP8

---
+++ board_with_heuristics.py
@@ -0,0 +1,296 @@
+import numpy as np
+import random
+
+
+class Board(object):
+
+    def __init__(self, board=None, points=0):
+        if board is None:
+            self.board = np.zeros([4, 4])
+            self.spawn_number()
+            self.spawn_number()
+        else:
+            self.board = np.copy(board)
+
+        if points == 0:
+            self.points = 0
+        else:
+            self.points = points
+
+    def make_move(self, direction, spawn=True):
+        """
+        Makes a move on the current board.
+        params:
+            direction: 'w':up, 'a':left, 's':down, 'd':right
+            board: 4x4 np array representing the board.
+        returns:
+            board: updated 4x4 np array that represents the board, after update
+        """
+        oldboard = np.copy(self.board)  # makes copy of the initial board state
+
+        if direction == "w":
+            # moving up
+            self.move_up()
+
+        if direction == "s":
+            # down
+            self.move_down()
+
+        if direction == "a":
+            # left
+            self.move_left()
+
+        if direction == "d":
+            # right
+            self.move_right()
+
+        if (len(np.where(self.board.reshape(-1) == 0)[0]) > 0 and not np.array_equal(self.board, oldboard)):
+            if spawn:
+                self.spawn_number()
+
+        self.points = self.rank_board()
+
+
+    def rank_board(self):
+        """
+        Uses Heuristics to rank the quality of the board. 
+        
+        returns:
+            
+            float: returns the output of the evaluation functin.
+        
+        
+        """
+        
+
+        x1 = 100 #weighting of highest cell being in top left
+        x2 = -2 #weighting on having many free cells
+        x3 = 5 #weighting of having many adjacent moves available
+        x4 = 75 #weighting of second highest cell being next to highest
+        x5 = 1  #weighting of achiving a good score
+        #x6 = 200 #weighting of merging the third largest cells
+
+        z1=0
+        i, j = self.argmax2d()
+        if (self.in_corner(i, j)):
+            z1 = 1
+
+        free_cells = np.where(self.board.reshape(-1) == 0)[0]
+        z2 = len(free_cells)
+
+        z3 = self.get_number_adjacent()
+        z4 = 0
+        z5 = self.points
+        locations_second_highest = np.argwhere(self.board == self.second_highest())
+        for h in locations_second_highest:
+            if (h[0] == 1 and h[1] == 0):
+                z4 = 1
+
+        return (x1*z1 + x3*z3 + x4*z4 + x5*z5 + x2*z2)
+
+    def second_highest(self):
+        flat = self.board.flatten()
+        flat.sort()
+        return (flat[-2])
+
+    def third_highest(self):
+        flat = self.board.flatten()
+        flat.sort()
+        return (flat[-3])
+
+    def get_number_adjacent(self):
+        number_adjacent = 0
+        number_adjacent += self.move_up(False)
+        number_adjacent += self.move_left(False)
+        return(number_adjacent)
+
+
+    def move_up(self, move=True):
+        alreadymerged = []
+        merge_counter = 0
+        self.points = 0
+
+        for _ in range(3):  # ensures all pieces move as far as they can
+            for i in range(1, 4, 1):
+                for j in range(4):
+                    height = i
+                    while height > 0:  # prevents tiles moving off screen.
+                        if self.board[i - 1][j] == 0 and move:
+                            self.board[i - 1][j] = self.board[i][j]
+                            self.board[i][j] = 0
+                        elif (self.board[i - 1][j] == self.board[i][j] and (
+                                str(i - 1) + "," + str(j)) not in alreadymerged and (
+                                      str(i) + "," + str(j)) not in alreadymerged):
+                            if move:
+
+                                self.board[i - 1][j] *= 2
+                                if (self.board[i][j] == self.third_highest()):
+                                    self.points+=100
+                                self.points += self.board[i - 1][j]
+                                self.board[i][j] = 0
+
+                                # logs tiles which have merged
+                                alreadymerged.append(str(i - 1) + "," + str(j))
+                                # logs tiles which have merged
+                                alreadymerged.append(str(i) + "," + str(j))
+                            merge_counter += 1
+
+                        height -= 1
+        return merge_counter
+
+    def move_down(self, move=True):
+        merge_counter = 0
+        alreadymerged = []
+        self.points = 0
+        for _ in range(3):
+            for i in range(3, -1, -1):
+                for j in range(4):
+                    height = i
+                    while height < 3:
+                        if self.board[i + 1][j] == 0 and move:
+                            self.board[i + 1][j] = self.board[i][j]
+                            self.board[i][j] = 0
+
+                        elif (self.board[i + 1][j] == self.board[i][j] and (
+                                str(i) + "," + str(j)) not in alreadymerged and (
+                                      str(i + 1) + "," + str(j)) not in alreadymerged):
+                            if move:
+                                self.board[i + 1][j] *= 2
+                                if (self.board[i][j] == self.third_highest()):
+                                    self.points += 100
+                                self.points += self.board[i + 1][j]
+                                self.board[i][j] = 0
+                                alreadymerged.append(str(i) + "," + str(j))
+                                alreadymerged.append(str(i + 1) + "," + str(j))
+                            merge_counter += 1
+                        height += 1
+        return merge_counter
+
+    def move_left(self, move=True):
+        alreadymerged = []
+        merge_counter = 0
+        self.points = 0
+        for _ in range(3):
+            for i in range(4):
+                for j in range(4):
+                    height = j
+                    while height > 0:
+                        if self.board[i][j - 1] == 0 and move:
+                            self.board[i][j - 1] = self.board[i][j]
+                            self.board[i][j] = 0
+
+                        elif (self.board[i][j - 1] == self.board[i][j] and (
+                                str(j) + "," + str(i)) not in alreadymerged and (
+                                      str(j - 1) + "," + str(i)) not in alreadymerged):
+
+                            if move:
+                                self.board[i][j - 1] *= 2
+                                if (self.board[i][j] == self.third_highest()):
+                                    self.points += 100
+                                self.points += self.board[i][j - 1]
+
+                                self.board[i][j] = 0
+                                alreadymerged.append(str(j) + "," + str(i))
+                                alreadymerged.append(str(j - 1) + "," + str(i))
+                            merge_counter += 1
+                        height -= 1
+        return merge_counter
+
+    def move_right(self, move=True):
+        alreadymerged = []
+        merge_counter = 0
+        self.points = 0
+        for _ in range(3):
+            for i in range(4):
+                for j in range(4, -1, -1):
+                    height = j
+                    while height < 3:
+                        if self.board[i][j + 1] == 0:
+                            self.board[i][j + 1] = self.board[i][j]
+                            self.board[i][j] = 0
+                        elif (self.board[i][j + 1] == self.board[i][j] and (
+                                str(j) + "," + str(i)) not in alreadymerged and (
+                                      str(j + 1) + "," + str(i)) not in alreadymerged):
+                            if move:
+                                self.board[i][j + 1] *= 2
+                                self.points += self.board[i][j + 1]
+                                if (self.board[i][j] == self.third_highest()):
+                                    self.points += 100
+                                self.board[i][j] = 0
+                                alreadymerged.append(str(j) + "," + str(i))
+                                alreadymerged.append(str(j + 1) + "," + str(i))
+                            merge_counter += 1
+                        height += 1
+        return merge_counter
+
+    def spawn_number(self, pick_random=True, rand=0, spawn_number=0):
+        """
+        Picks a random empty cell to spawn a number into it.
+        params:
+            board: 4x4 np array representing the board.
+        returns:
+            board: updated 4x4 np array that represents the board, after update
+        """
+        free_cells = np.where(self.board.reshape(-1) == 0)[0]  # list containing indexes of 0's
+        if len(free_cells) > 0:
+            # picks a random free spaces to spawn
+            if pick_random:
+                rand = random.randint(0, len(free_cells) - 1)
+            if spawn_number == 0:
+                if random.random() < 0.9:
+                    # 90% of the time it will spawn a 2
+                    self.board.reshape(-1)[free_cells[rand]] = 2
+                else:
+                    self.board.reshape(-1)[free_cells[rand]] = 4
+            else:
+                self.board.reshape(-1)[rand] = spawn_number
+
+    def free_cells(self):
+        """
+        Returns the number of free cells, to be used for the AI.
+        """
+        free_cells = np.where(self.board.reshape(-1) == 0)[0]  # list containing indexes of 0's
+        return free_cells
+
+    def in_corner(self, i, j):
+        if ((i == 0 and j == 0)):
+            return (True)
+        else:
+            return (False)
+
+    def argmax2d(self):
+        n, m = self.board.shape
+        x_ = np.ravel(self.board)
+        k = np.argmax(x_)
+        i, j = k // m, k % m
+        return i, j
+
+    def moves_available(self, get_moves=False):
+        """
+        Determines whether a move can be made.
+        params:
+            board: 4x4 np array representing the board.
+        returns:
+            boolean: True if moves are available or False if not.
+        """
+
+        # list containing indexes of 0's
+        free_cells = np.where(self.board.reshape(-1) == 0)[0]
+
+        if len(free_cells) != 0 and get_moves is False:
+            return True
+
+        possible_moves = []
+        for move in ["w", "s", "a", "d"]:
+            initialboard = Board(self.board)
+            initialboard.make_move(move)
+            if not np.array_equal(initialboard.board, self.board):
+                possible_moves.append(move)
+            del initialboard
+
+        if get_moves:
+            return possible_moves
+
+        if len(possible_moves) > 0:
+            return True
+        return False
---
+++ generate_expectimax_data.py
@@ -0,0 +1,156 @@
+from board_with_heuristics import Board
+import os
+from termcolor import colored
+
+
+def cls():
+    """
+    Clears the console (used to make it prettier, not needed in reality)
+    """
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def beautify_print(board):
+    """
+    Prints the board in a prettier way.
+    params:
+        board: 4x4 np array representing the board.
+    """
+
+    color_dict = {0: "red", 2: "green", 4: "yellow", 8: "blue", 16: "magenta", 32: "cyan", 64: "green",
+                  128: "yellow", 256: "blue", 512: "magenta", 1024: "cyan", 2048: "green", 5096: "yellow"}
+    print("==================")
+    for i in range(4):
+        print("|", end=" ")
+        for j in range(3):
+            try:
+                print(
+                    colored(str(int(board[i][j])), color_dict[int(board[i][j])]), end=" | ")
+            except:
+                print(board[i][j], end=" | ")
+        try:
+            print(colored(str(int(board[i][3])),
+                          color_dict[int(board[i][3])]), end=" |\n")
+        except:
+            print(board[i][3], end=" |\n")
+        print("==================")
+
+
+def find_children(board, is_initial=False):
+    """
+    Find children of a given parent node. Simulates each move (w,a,s,d), and then finds all possible spawn
+    possibilities. All these possibilities are considered children of the parent node.
+    Parameters:
+        board: the parent node being a Board object
+        is_initial: Boolean value, to be used in the case we do not want to
+                    simulate all possible moves, but just find all possible
+                    spawn locations, to be used for depth 1 cases.
+    Returns:
+        A list of a all children nodes represented by Board objects, List(Board)
+    """
+    parents = []
+    children = []
+    if not is_initial:
+        for move in board.moves_available(True):
+            moved_board = Board(board.board, board.points)
+            moved_board.make_move(move, False)
+            parents.append(moved_board)
+            del moved_board
+    else:
+        parents = [board]
+    for parent in parents:
+        for cell_index in parent.free_cells():
+            for two_four in [2]:
+                spawned_board = Board(parent.board, parent.points)
+                spawned_board.spawn_number(pick_random=False, rand=cell_index, spawn_number=two_four)
+                children.append(spawned_board)
+                del spawned_board
+    return children
+
+
+if __name__ == "__main__":
+    
+    for i in range(5000):
+
+        board1 = Board()  # Initialise a board
+    
+        cls()  # using clear function here as it seems that not using it would corrupt the output
+    
+        print("Board initialised:")
+    
+        while board1.moves_available():
+            # cls() # comment this out if you want the program to print out everything
+            #beautify_print(board1.board)
+            depth = 0
+            max_depth = 2
+            tree = {"w": [[], []], "a": [[], []], "s": [[], []], "d": [[], []]}  # {move:[[parents][children]]}
+            available_moves = board1.moves_available(True)  # Gets a list of all possible moves
+    
+            while depth < max_depth:
+    
+                # Generate all children nodes
+                if depth == 0:  # Initialising the tree dictionary with depth 1 children
+                    for move in available_moves:
+                        moved_board = Board(board1.board, board1.points)
+                        moved_board.make_move(move, False)
+                        tree[move][1] += find_children(board=moved_board, is_initial=True)
+                        del moved_board
+                else:
+                    for move in available_moves:
+                        for parent in tree[move][0]:
+                            tree[move][1] += find_children(board=parent)
+                depth += 1
+    
+                # Determine updated average score for each move
+                average_scores = []
+                for move in available_moves:
+                    tree[move][0] = tree[move][1]
+                    tree[move][1] = []
+                    average_scores.append(0)
+                    for i in range(0, len(tree[move][0]), 2):
+                        # Probability of 2 * number of points
+                        average_scores[-1] += (1.0 * tree[move][0][i].points)
+                        # Probability of 4 * number of points
+                        # average_scores[-1] += (0.1 * tree[move][0][i+1].points)
+                        # 0.5 because we are dividing by the (number of children / 2),
+                        # due to multiplying the number of point with the probability
+                        # of having a 2 and a 4 per tile.
+                    if len(tree[move][0]) > 0:
+                        average_scores[-1] /= (len(tree[move][0]))
+                    else:
+                        average_scores[-1] = 0
+                    
+                    # Determines best move
+                best_move = []
+                best_move_avg = 0
+                for move in range(len(available_moves)):
+                    current_move_avg = average_scores[move]
+                    if best_move_avg == current_move_avg:
+                        best_move.append(available_moves[move])
+                        best_move_avg = current_move_avg
+                    elif best_move_avg < current_move_avg:
+                        best_move = [available_moves[move]]
+                        best_move_avg = current_move_avg
+                compare_moves = available_moves[:]
+                if len(available_moves) == 1:
+                    print(str(best_move[0] + "," + str(board1.board.flatten())))
+                    file1 = open("exdata.txt","a") 
+                    
+                    # Writes to training data text file
+                    file1.write(str(best_move[0] + "," + str(board1.board.flatten().tolist())) + "\n") 
+                    file1.close() 
+                    board1.make_move(best_move[0])
+                    break
+                if depth == max_depth:
+                    file1 = open("exdata.txt","a")#append mode 
+                    
+                    # Writes to training data text file
+                    file1.write(str(best_move[0] + "," + str(board1.board.flatten().tolist())) + "\n") 
+                    file1.close()
+                    board1.make_move(best_move[0])
+                    
+                     
+    
+    cls()
+    #beautify_print(board1.board)
+    print("Final score: " + str(board1.points))
